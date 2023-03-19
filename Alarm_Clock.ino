e83f9b51d38845a0f928b4cda7e6a3bddca62daa
Patryk Wrzalek <wrzalek.patryk@gmail.com>
2023-03-19 22:51:55 +0100

    Added functions lcd_time_set_manual which set time by potentiometer and button

--- Alarm_Clock.ino
+++ Alarm_Clock.ino
@@ -1,7 +1,8 @@
 // Alarm_Clock on Arduino Uno//
 
-#include <LiquidCrystal.h>           //Libraries attached
-#define PRESCALER2 7812              //"User Prescaler"
+#include <LiquidCrystal.h> //Libraries attached
+#define PRESCALER2 7812    //"User Prescaler"
+#define potentiometer A0
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7); // PINs LCD are connected
 uint8_t time_sec = 0, time_min = 0, time_h = 0, one_sec = 0;
 volatile uint8_t state = 0;
@@ -48,7 +49,7 @@ void lcd_print_time(uint8_t(*time_sec), uint8_t(*time_min), uint8_t(*time_h))
   }
 }
 
-void lcd_time_set(uint8_t(*time_h), uint8_t(*time_min))
+void lcd_time_set_by_serial(uint8_t(*time_h), uint8_t(*time_min))
 {
   Serial.print("Set hour: ");
   uint8_t time_h2[2] = {0x00, 0x00};
@@ -84,13 +85,43 @@ void lcd_time_set(uint8_t(*time_h), uint8_t(*time_min))
   state = 3;
 }
 
+void lcd_time_set_manual(uint8_t(*time_h), uint8_t(*time_min)) // time seting by poten and buttons
+{
+  bool button_check = 0;
+  Serial.print("Set hour: ");
+  while (!button_check) // wait until user press set_button
+  {
+    (*time_h) = analogRead(potentiometer) * 6 / 256; // 24/1024=6/256
+    lcd.setCursor(4, 1);
+    if ((*time_h) < 10)
+      lcd.print("0");
+    lcd.print(*time_h);
+    if (!(PINB & (1 << PB1))) // check if set_button was pressed
+      button_check = 1;
+  }
+  button_check = 0;
+  Serial.print("Set minutes: ");
+  waiting_loop();       // waiting loop which gives time to user for release set_button
+  while (!button_check) // wait until user press set_button
+  {
+    (*time_min) = analogRead(potentiometer) * 15 / 256; // 60/1024=15/256
+    lcd.setCursor(7, 1);
+    if ((*time_min) < 10)
+      lcd.print("0");
+    lcd.print(*time_min);
+    if (!(PINB & (1 << PB1))) // check if set_button was pressed
+      button_check = 1;
+  }
+  state = 3; // go to next state
+}
+
 void waiting_loop() // Waiting loop
 {
   for (long int i = 0; i < (F_CPU / 16); i++) // F_CPU -Arduino work frequancy
     asm("");
 }
 
-int main()
+void setup()
 {
   lcd.begin(16, 2); // Settings for LCD
   lcd.clear();
@@ -100,11 +131,17 @@ int main()
 
   Serial.begin(115200);
 
-  TCCR1A = 0x00;      // Set WGM bits to CTC (mode 4)
-  TCCR1B = 0x0D;      // Set CS bits to prescaler of 1024
+  DDRB = 0b00000000;  // Initialize PINs which we used from PORT C
+  PORTB = (1 << PB1); // Enable internal pullup resistor to INPUT PC0, PC1, PC2
+
+  pinMode(potentiometer, INPUT);
+
+  TCCR1A = 0x00;      // Reset timer1 control regA
+  TCCR1B = 0x0D;      // Set CS bits to prescaler of 1024 and set WGM bits to CTC (mode 4)
+  TCNT1 = 0x00;       // Reset timer1
   OCR1A = PRESCALER2; // Set output compare for interrupt period of 1sec
   TIMSK1 = 0x02;      // Enable output compare match interrupt A
-  sei();              // Enable interrupts
+  sei();              // Enable global interrupts
 
   /*for (int y=0; y<20; y++) //Checking the rounding performed by abs()
   {
@@ -113,25 +150,27 @@ int main()
     Serial.print(". ");
     Serial.println(x);
   }*/
-  while (1)
+}
+void loop()
+{
+  if (state == 3)
   {
-    if (state == 3)
-    {
-      lcd.setCursor(0, 0);
-      lcd.print(" Measured time: ");
-      lcd_print_time(&time_sec, &time_min, &time_h);
-    }
-    else if (state == 2)
-      lcd_time_set(&time_h, &time_min);
-    else
-    {
-      lcd.setCursor(0, 0);
-      lcd.print("    Set time:   ");
-      lcd.setCursor(0, 3);
-      lcd.print("    HH:MM:SS    ");
-      waiting_loop();
-      state = 2;
-    }
+    lcd.setCursor(0, 0);
+    lcd.print(" Measured time: ");
+    lcd_print_time(&time_sec, &time_min, &time_h);
+  }
+  else if (state == 2)
+  {
+    lcd_time_set_manual(&time_h, &time_min);
+  }
+  else
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("    Set time:   ");
+    lcd.setCursor(0, 3);
+    lcd.print("    HH:MM:SS    ");
+    waiting_loop();
+    state = 2;
   }
 }
 
